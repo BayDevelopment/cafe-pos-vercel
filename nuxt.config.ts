d41feaaa0ab1f53820c1e5a95c2a85a76d71a18b
BayDevelopment <email-dayxcode@gmail.com>
2026-08-30 15:47:01 +0700

    fix: remove prisma inline config to resolve wasm module not found

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,12 +15,5 @@ export default defineNuxtConfig({
   supabase: {
     redirect: false,
   },
-  nitro: {
-    experimental: {
-      wasm: true,
-    },
-    externals: {
-      inline: ['@prisma/client'],
-    },
-  },
-});
+  // Hapus blok nitro sepenuhnya
+})
