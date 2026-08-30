64b7c2dcf8d0d9bb59b8ea8f65a98d5aa946d039
BayDevelopment <email-dayxcode@gmail.com>
2026-08-30 20:48:04 +0700

    fix: kembalikan polyfill __dirname untuk kompatibilitas queryCompiler dengan ESM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,18 @@ export default defineNuxtConfig({
   ],
   nitro: {
     preset: 'vercel',
+    rollupConfig: {
+      output: {
+        intro: `
+          import { createRequire as __prismaCreateRequire } from 'module';
+          import { fileURLToPath as __prismaFileURLToPath } from 'url';
+          import { dirname as __prismaDirname } from 'path';
+          const require = __prismaCreateRequire(import.meta.url);
+          const __filename = __prismaFileURLToPath(import.meta.url);
+          const __dirname = __prismaDirname(__filename);
+        `,
+      },
+    },
   },
   supabase: {
     redirect: false,
